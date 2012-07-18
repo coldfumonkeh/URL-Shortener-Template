7a6646bf730a51146e169bfe3cfc6662c5af3353
Matt Gifford <me@mattgifford.co.uk>
2012-07-18 11:44:26 +0100

    Initial commit for template

---
+++ Rakefile
@@ -0,0 +1,8 @@
+# encoding: utf-8
+require 'rubygems'
+require 'bundler'
+#Bundler::GemHelper.install_tasks
+
+task :default do
+  puts "Default task running for this build"
+end
---
+++ environment.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'data_mapper'
+require 'json'
+require 'ostruct'
+require 'rake'
+
+require 'sinatra' unless defined?(Sinatra)
+
+configure do
+
+  SiteConfig = OpenStruct.new(
+          :title => 'URL Shortener',
+          :author => 'Your Name Can Go Here',
+          :url_base => 'http://localhost:4567/' # the url of your application
+        )
+        
+  # load models
+  $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+  Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
+  
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/shorturls.db")
+
+end
